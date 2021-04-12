3e999fa7f0fff556986c0e85221110efe47de9ec
Jake Dickens <Lcjacobpd@gmail.com>
2021-04-12 17:54:51 -0600

    Zombie Hoard Approaches
    Added save-to-file support for encounters
    Removed ability to edit encounters
    Added "Small Zombie Hoard" default encounter

--- html/table.js
+++ html/table.js
@@ -78,18 +78,18 @@ function deletePlayer(name) {
 
 function savePBook() {
     text = 'var Players = [\n'
-    text += '\t//Player Name,           Character Name, 	     Class,        HP\n'
+    text += '\t//Player Name,           Character Name, 	        Class,        HP\n'
 
     for (var p = 0; p < Players.length; p++) {
-        text += '\t['
-        text += '\'' + Players[p][0].padEnd(20) + '\', '
-        text += '\'' + Players[p][1].padEnd(20) + '\', \t'
+        text += '\t[';
+        text += '\'' + Players[p][0].padEnd(20) + '\', ';
+        text += '\'' + Players[p][1].padEnd(20) + '\', \t';
         
-        text += '\'' + Players[p][2].padEnd(10) + '\', ' // Class
-        text += Players[p][3] + ', ' // HP
-        text += '],\n'
+        text += '\'' + Players[p][2].padEnd(10) + '\', '; // Class
+        text += Players[p][3]; // HP
+        text += '],\n';
     }
-    text += ']'
+    text += ']';
 
     console.log(text)
 
@@ -210,26 +210,26 @@ function saveMBook() {
     text += '\t//Name,                  Condition,\tAC, HP, \tSTR, DEX, CON,\tINT, WIZ, CHA,\tHit Dice,     Resistance\n'
 
     for (var m = 0; m < MonsterBook.length; m++) {
-        text += '\t['
-        text += '\'' + MonsterBook[m][0].padEnd(20) + '\', '
-        text += '\'' + MonsterBook[m][1] + '\', \t'
+        text += '\t[';
+        text += '\'' + MonsterBook[m][0].padEnd(20) + '\', ';
+        text += '\'' + MonsterBook[m][1] + '\', \t';
 
-        text += MonsterBook[m][2] + ', ' // AC
-        text += MonsterBook[m][3] + ',  \t' // HP
+        text += MonsterBook[m][2] + ', '; // AC
+        text += MonsterBook[m][3] + ',  \t'; // HP
 
-        text += MonsterBook[m][4] + ', ' // STR
-        text += MonsterBook[m][5] + ', ' // DEX
-        text += MonsterBook[m][6] + ',  \t' // CON
+        text += MonsterBook[m][4] + ', '; // STR
+        text += MonsterBook[m][5] + ', '; // DEX
+        text += MonsterBook[m][6] + ',  \t'; // CON
 
-        text += MonsterBook[m][7] + ', ' // INT
-        text += MonsterBook[m][8] + ', ' // WIZ
-        text += MonsterBook[m][9] + ',\t\t' // CHA
+        text += MonsterBook[m][7] + ', '; // INT
+        text += MonsterBook[m][8] + ', '; // WIZ
+        text += MonsterBook[m][9] + ',\t\t'; // CHA
         
-        text += '\'' + MonsterBook[m][10] + '\',  \t'
-        text += '\'' + MonsterBook[m][11] + '\''
-        text += '],\n'
+        text += '\'' + MonsterBook[m][10] + '\',  \t';
+        text += '\'' + MonsterBook[m][11] + '\'';
+        text += '],\n';
     }
-    text += ']'
+    text += ']';
 
     console.log(text)
 
@@ -254,12 +254,12 @@ function saveMBook() {
  * Encounter Controls
  *********************/
 
-var encounters = [];
-
 function saveEncounter() {
     var form = document.getElementById("encounterform").elements;
     var notes = document.getElementById("encounter-notes").value;
     var monsters = document.getElementById("monster-tb").value;
+
+    monsters = monsters.replace(/\s/g, '');
 
     var encounter = [
         form['name'].value,
@@ -279,7 +279,7 @@ function saveEncounter() {
         return;
     }
 
-    encounters.push(encounter);
+    Encounters.push(encounter);
     addEncounterRow(encounter);
 
     document.getElementById("encounterform").reset(); 
@@ -304,9 +304,9 @@ function addEncounterRow(encounter) {
 function getEncounter(name) {
     // Fetch encounter by name
     var row;
-    for (var p = 0; p < encounters.length; p++)
-        if (encounters[p][0] == name) {
-            encounter = encounters[p];
+    for (var p = 0; p < Encounters.length; p++)
+        if (Encounters[p][0] == name) {
+            encounter = Encounters[p];
             row = p;
         }
 
@@ -316,11 +316,41 @@ function getEncounter(name) {
 function deleteEncounter(name) {
     var row = getEncounter(name);
     document.getElementById('encounterlist').deleteRow(row + 1);
-    encounters.splice(row, 1);
+    Encounters.splice(row, 1);
+}
+
+function saveEBook() {
+    text = 'var Encounters = [\n'
+    text += '\t//Name,                  Notes,     Monsters\n'
+
+    for (var e = 0; e < Encounters.length; e++) {
+        text += '\t[';
+        text += '\'' + Encounters[e][0].padEnd(20) + '\', ';
+
+        text += '\'' + Encounters[e][1] + '\', \t'; // Notes
+        text += '\'' + Encounters[e][2] + '\''; // Monsters
+        text += '],\n';
+    }
+    text += ']';
+
+    const textToBlob = new Blob([text], {type: 'text/plain' });
+    const uFileName = 'encounters.js';
+
+    let newLink = document.createElement("a");
+    newLink.download = uFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBlob);
+    } else {
+        newLink.href = window.URL.createObjectURL(textToBlob);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+    newLink.click();
 }
 
 function playEncounter(name) {
     var row = getEncounter(name);
-    var encounter = encounters[row];
-
+    var encounter = Encounters[row];
+    console.log(encounter);
 }
