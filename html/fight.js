9c3d827c2ec1983bc748d66d9e461c7eab72fb10
Jake Dickens <Lcjacobpd@gmail.com>
2021-05-11 14:32:33 -0600

    Challanger Fix
    Fixed id assignment when adding monsters during combat; offset
    from existing list length. Challangers now considered when "next"
    is called.
    Absorbed mobs.js as global in fight.js
    Redundant code cleanup in fight.js
    Small Zombie Hoard -> Undead Hoard

--- html/fight.js
+++ html/fight.js
@@ -1,15 +1,28 @@
-GENERIC_STYLE = "color: #b9bbbe; border-left: 4px solid #b9bbbe";
-ACTIVE_STYLE = "color: #43b581; border-left: 8px solid #43b581";
-KO_STYLE = "border-left: 4px solid #faa61a";
-DEAD_STYLE = "border-left: 4px solid #f04747";
-CURRENT = 0;
-MON_HP = 0;
+// Style Constants
+var GENERIC_STYLE   = "color: #b9bbbe; border-left: 4px solid #b9bbbe";
+var ACTIVE_STYLE    = "color: #43b581; border-left: 8px solid #43b581";
+var KO_STYLE        = "border-left: 4px solid #faa61a";
+var DEAD_STYLE      = "border-left: 4px solid #f04747";
+
+// Combatant Constants
+var Mobs    = [];
+var CURRENT = 0;
+var MON_HP  = 0;
+
+var Challengers = []; // Appended during fight
+
 
 /**********************
- * Run Encounter
+ * Utility Functions
  *********************/
+function slayChildren(parentname) {
+    var parent = document.getElementById(parentname);
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
- function status_box(id) {
+function status_box(id) {
     return '\
         <select id="st' +id+ '" type="text"> \
             <option value="normal">normal</option> \
@@ -33,15 +46,101 @@ MON_HP = 0;
     '
 }
 
- function slayChildren(parentname) {
-    var parent = document.getElementById(parentname);
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
+function addMonFighter(m, offset=0) {
+    if (offset == 0) {
+        var names = Mobs[m].split(',');
+        var mon = getMonster(names[1]);
     }
+        
+    else {
+        var names = Challengers[m];
+        var mon = getMonster(names);
+    }
+
+    var num = Number(m) + offset;
+    
+
+    // Setup html elements
+    var element = document.createElement("box");
+    var left = document.createElement("left")
+    var right = document.createElement("right")
+
+    var hp = document.createElement("input");
+    var stat = document.createElement("st");
+    stat.innerHTML = status_box(num);
+
+    // Get monster details
+    
+
+    left.innerHTML = "NPC " + mon[0];
+    right.innerHTML = "<img src='img/detail.svg' onclick='viewMonster(\"" +mon[0]+ "\")'>";  // Details popup icon
+    right.innerHTML += '<p>' + mon[2] + '</p>';     // AC
+    
+    hp.value = mon[3];
+    hp.id = "hp"+num;
+    MON_HP += mon[3];
+
+    // Add elements to box
+    right.appendChild(hp)
+    element.appendChild(left);
+    element.appendChild(right)
+    right.appendChild(stat);
+
+    element.id = "box"+num;
+    document.getElementById("fighters").appendChild(element)
+    document.getElementById("st"+num).value = mon[1];
 }
 
-function rollInit() {
-    // Propogate page with combatants
+function addPlrFighter(m) {
+    var details = Mobs[m].split(',');
+
+    var element = document.createElement("box");
+    var left = document.createElement("left")
+    var right = document.createElement("right")
+
+    var hp = document.createElement("input");
+    var stat = document.createElement("st");
+    stat.innerHTML = status_box(m);
+
+    // Get player details
+    var perp = getPlayer(details[0])
+
+    left.innerHTML = perp[0] +" "+ perp[1];
+    
+    hp.value = perp[3];
+    hp.id = "hp"+m;
+    right.appendChild(hp)
+
+    // Add elements to box
+    element.appendChild(left);
+    element.appendChild(right)
+    right.appendChild(stat);
+
+    element.id = "box"+m;
+    document.getElementById("fighters").appendChild(element)
+}
+
+
+/**********************
+ * Setting up a Fight
+ *********************/
+
+function playEncounter(name) {
+    // Reset global var and roll init page
+    Mobs = [];
+    slayChildren("people");
+
+    // Collect players
+    for (var p = 0; p < Players.length; p++)
+        Mobs.push(Players[p].slice(0,3).join());
+
+    // Collect relevant monsters
+    var row = getEncounterID(name);
+    var creeps = Encounters[row][2].split(',');
+    for (var c = 0; c < creeps.length; c++)
+        Mobs.push("NPC," + creeps[c] + ", ");
+
+    // Populate init page with combatants
     for(var m = 0; m < Mobs.length; m++) {
         var element = document.createElement("box");
         var details = Mobs[m].split(',');
@@ -61,96 +160,50 @@ function rollInit() {
         element.innerHTML += "<input type='number' id='roll"+m+"' placeholder='roll' style='margin-right: 0;'/>";
         document.getElementById("people").appendChild(element)
     }
-}
 
-function loadFight() {
-    for(var m = 0; m < Mobs.length; m++) {
-        var details = Mobs[m].split(',');
-
-        var element = document.createElement("box");
-        var left = document.createElement("left")
-        var right = document.createElement("right")
-
-        var hp = document.createElement("input");
-        var stat = document.createElement("st");
-
-        stat.innerHTML = status_box(m);
-
-        if (details[0] == "NPC") {
-            var mon = getMonster(details[1]);
-
-            left.innerHTML = "NPC " + mon[0];
-            right.innerHTML = "<img src='img/detail.svg' onclick='viewMonster(\"" +mon[0]+ "\")'>";  // Details popup icon
-            right.innerHTML += '<p>' + mon[2] + '</p>';     // AC
-            
-            hp.value = mon[3];
-            hp.id = "hp"+m;
-            right.appendChild(hp)
-
-            MON_HP += mon[3];
-
-        } else {
-            var perp = getPlayer(details[0])
-
-            left.innerHTML = perp[0] +" "+ perp[1];
-            
-            hp.value = perp[3];
-            hp.id = "hp"+m;
-            right.appendChild(hp)
-        }
-
-        element.appendChild(left);
-        element.appendChild(right)
-        right.appendChild(stat);
-
-        element.id = "box"+m;
-        document.getElementById("fighters").appendChild(element)
-
-        if (details[0] == "NPC")
-            document.getElementById("st"+m).value = mon[1];
-    }
-}
-
-function playEncounter(name) {
-    Mobs = [];
-    slayChildren("people");
-
-    // Collect relevant Monsters and Players
-    for (var p = 0; p < Players.length; p++)
-        Mobs.push(Players[p].slice(0,3).join());
-
-    var row = getEncounterID(name);
-    var creeps = Encounters[row][2].split(',');
-    for (var c = 0; c < creeps.length; c++)
-        Mobs.push("NPC," + creeps[c] + ", ");
-
-    rollInit();
+    // Display init page
     showTab(7, 8);
 }
-
 
 function sortFighters() {
     cache = [];
     MON_HP = 0;
+
+    // Pair combatants with their roll
     for(var m = 0; m < Mobs.length; m++) {
         var roll = parseInt(document.getElementById("roll"+m).value);
         cache.push([roll, Mobs[m]]);
     }
 
+    // Sort cached pair and push to global
     cache.sort(function(a, b){return b[0]-a[0]});
     Mobs = []
     for(item in cache) {
         Mobs.push(cache[item][1])
     }
 
+    // Clear old fighters list
     slayChildren("fighters");
-    loadFight()
-    showTab(9, 10);
 
+    // Populate new fighters list
+    for(var m = 0; m < Mobs.length; m++) {
+        var details = Mobs[m].split(',');
+        if (details[0] == "NPC")
+            addMonFighter(m);
+        else
+            addPlrFighter(m);
+    }
+
+    // Highlight first/active combatant
     CURRENT = 0;
-    
     document.getElementById("box0").style = ACTIVE_STYLE;
+    showTab(9, 10);
 }
+
+
+/**********************
+ * During up a Fight
+ *********************/
 
 function next() {
     pos = CURRENT;
@@ -209,44 +262,13 @@ function next() {
 
 
 function addMonsters() {
-    var challengers = document.getElementById("monster-ch").value.split(',');;
-    var fighters = document.getElementById("fighters");
+    var offset = document.getElementById("fighters").children.length;
+    Challengers = document.getElementById("monster-ch").value.split(',');
 
-    for (var m in challengers) {
-        var mon = getMonster(challengers[m]);
-        
-
-        var element = document.createElement("box");
-        var left = document.createElement("left")
-        var right = document.createElement("right")
-
-        var hp = document.createElement("input");
-        var stat = document.createElement("st");
-        stat.innerHTML = status_box(m);
-
-
-        left.innerHTML = "NPC " + mon[0];
-        right.innerHTML = "<img src='img/detail.svg' onclick='viewMonster(\"" +mon[0]+ "\")'>";  // Details popup icon
-        right.innerHTML += '<p>' + mon[2] + '</p>';     // AC
-        
-        hp.value = mon[3];
-        hp.id = "hp"+m;
-        right.appendChild(hp)
-
-        MON_HP += mon[3];
-
-
-        element.appendChild(left);
-        element.appendChild(right)
-        right.appendChild(stat);
-
-        element.id = "box"+m;
-        fighters.appendChild(element)
-
-        document.getElementById("st"+m).value = mon[1];
-
-
-        // Close "Add Monster" tab
-        document.getElementById("challenger").style.visibility = "hidden";
+    for (var m in Challengers) {
+        Mobs.push("NPC," + Challengers[m] + ", ");
+        addMonFighter(m, offset);
     }
+    
+    document.getElementById("challenger").style.visibility = "hidden";
 }
