5d84c99337b66e007fe52930218522b9d7f2fc95
Jake Dickens <Lcjacobpd@gmail.com>
2021-05-20 09:02:21 -0600

    Softer Popups
    Added fade transitions to popups and blackout element.

--- js/toast.js
+++ js/toast.js
@@ -16,20 +16,24 @@ function badTab() {
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
 
+function blackout() {
+    document.getElementById("blackout").className = "show";
+}
+
 function showPop(num) {
     for (var t = 1; t < 3; t++) {
         var pop = document.getElementById('pop'+t);
-        var vis = pop.style.visibility;
+        var vis = pop.className;
 
         // Toggle visiblity of current
         if (t == num)
-            if (vis == "visible")
-                pop.style.visibility = "hidden";
+            if (vis == "")
+                pop.className = "show";
             else
-                pop.style.visibility = "visible";
+                pop.className = "";
 
         // Hide all others
         else
-            pop.style.visibility = "hidden";
+            pop.className = "";
     }
 }
